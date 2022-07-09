d4d5b0f0582f81a4624eb31d85bab8b8abc0f8b1
Letam Bossman Barinua <letambarinua@gmail.com>
2022-07-09 13:01:08 +0100

    Create new Function createDivSquares

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let markTracker = [];
-
-const createDivs = (function(i) {
-    const gameboard = document.querySelector('.gameboard');
-    const square = document.createElement('div');
-    square.style.border = '1px solid black';
-    square.setAttribute('data-index-number', `${i}`);
-    square.classList.add('squareDivs', 'square');
-    gameboard.appendChild(square);
-  });
-  for (let i = 0; i < 9; i++) createDivs(i);
+const createDiv = function (i) {
+  const gameboard = document.querySelector(".gameboard");
+  const square = document.createElement("div");
+  square.style.border = "1px solid black";
+  square.setAttribute("data-index-number", `${i}`);
+  square.classList.add("squareDivs", "square");
+  gameboard.appendChild(square);
+};
+const createDivSquares = (function () {
+  for (let i = 0; i < 9; i++) createDiv(i);
+})();

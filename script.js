5e725ffd743e163938cc9bb0b3c4ed99da1dbf28
Letam Bossman Barinua <letambarinua@gmail.com>
2022-07-09 12:49:55 +0100

    Add createDivs function for creating Gameboard divs when invoked

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Tic Tac Toe');
+let markTracker = [];
+
+const createDivs = (function(i) {
+    const gameboard = document.querySelector('.gameboard');
+    const square = document.createElement('div');
+    square.style.border = '1px solid black';
+    square.setAttribute('data-index-number', `${i}`);
+    square.classList.add('squareDivs', 'square');
+    gameboard.appendChild(square);
+  });
+  for (let i = 0; i < 9; i++) createDivs(i);
